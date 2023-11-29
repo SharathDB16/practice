37ba68ed94e6fbef255f21f51c27f744184c90a1
Sharath DB <sharath.db@sugarboxnetworks.com>
2023-11-29 19:15:10 +0530

    test

--- testRoutes.py
+++ testRoutes.py
@@ -1,6 +1,15 @@
-from app import *
+from app import app
 
-def test_Home():
-    response = app.test_client().get('/')
+def test_home():
+    # Setup: Create a test client
+    test_client = app.test_client()
+
+    # Exercise: Perform the request
+    response = test_client.get('/')
+
+    # Verify: Check the response
     assert b"Welcome to Jenkins Tutorials" in response.data
     assert response.status_code == 200
+
+    # Teardown: (Optional) Clean up any resources used during the test
+    # This is automatically called if you use a `with` statement for the test client
